42538fbf85eb1c74f16fa065e205fa4b804e3e3d
dtieshue <dtieshue@gmail.com>
2026-03-29 00:02:38 -0400

    Redeploy latest build

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"c13fc7cb-b48c-4ada-8fc5-80d545dbb5a6","remote_package_size":36664,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":36664,"audio":false}]});
+  loadPackage({"package_uuid":"54a9fb1e-830f-4d91-be6b-6d9b37d7f0df","remote_package_size":36664,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":36664,"audio":false}]});
 
 })();
