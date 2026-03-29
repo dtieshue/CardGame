4ae6310ce5482c8f46bfb85fbfab1ef27cdf34dc
dtieshue <dtieshue@gmail.com>
2026-03-28 23:58:32 -0400

    Fix card animation: smooth lerp, no spazzing

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"89e872d7-e94a-4f25-8196-f39968d40f7a","remote_package_size":36734,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":36734,"audio":false}]});
+  loadPackage({"package_uuid":"c13fc7cb-b48c-4ada-8fc5-80d545dbb5a6","remote_package_size":36664,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":36664,"audio":false}]});
 
 })();
